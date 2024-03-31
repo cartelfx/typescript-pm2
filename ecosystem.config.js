557e433161b86b63d1875888978d6c624b3d0f25
cartel <119452317+cartelfx@users.noreply.github.com>
2024-03-31 15:31:10 +0400

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,19 +13,6 @@ module.exports = {
                 NODE_ENV: 'production'
             }
         },
-        {
-            name: 'stats',
-            script: 'dist/stats/index.js',
-            autorestart: true,
-            watch: false,
-            max_memory_restart: '1G',
-            env: {
-                NODE_ENV: 'development'
-            },
-            env_production: {
-                NODE_ENV: 'production'
-            }
-        },
         // other apps add
     ]
 };
