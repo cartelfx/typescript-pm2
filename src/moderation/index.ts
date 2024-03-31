f5c128abd8484b0b8bffccee55a1e120506a866b
cartel <119452317+cartelfx@users.noreply.github.com>
2024-03-31 13:59:33 +0300

    pm2 ts

---
+++ src/moderation/index.ts
@@ -0,0 +1,6 @@
+function log(name: string) {
+    return `Merhaba, ${name}!`;
+}
+
+const msg = log("Dünya");
+console.log(msg);
---
+++ src/stats/index.ts
@@ -0,0 +1,5 @@
+function greet(name: string) {
+    return `Merhaba, ${name}!`;
+}
+const message = greet("Dünya");
+console.log(message);
